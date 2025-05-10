6b149bc15304721b8b91d0c5cb4bdd8edc35d2be
Maxgaming060 <maxgaming.x.max@gmail.com>
2025-05-10 17:41:05 +0530

    Add files via upload

--- src/commands/admin/deny.js
+++ src/commands/admin/deny.js
@@ -3,19 +3,17 @@ const Embed = Utils.Embed;
 const { config, lang } = Utils.variables;
 
 module.exports = {
-    name: "accept",
-    run: async (bot, messageOrInteraction, args, { prefixUsed, member, guild, reply }) => {
+    name: "deny",
+    run: async (bot, messageOrInteraction, args, { prefixUsed, type, member,  guild, reply }) => {
         return new Promise(async resolve => {
             let id = args[0];
 
             if (!id) {
                 reply(Embed({
                     preset: "error",
-                    description: lang.AdminModule.Commands.Accept.NoID,
+                    description: lang.AdminModule.Commands.Deny.NoID,
                     usage: module.exports.usage
-                }, { prefixUsed }), {
-                    ephemeral: true
-                });
+                }, { prefixUsed }), { ephemeral: true });
 
                 return resolve();
             }
@@ -32,31 +30,32 @@ module.exports = {
     
                 const update = require("../../modules/methods/updateSuggestion");
                 const suggestionMessage = await guild.channels.cache.get(suggestion.channel)?.messages.fetch(suggestion.message);
-                const reason = args.slice(1).join(" ");
+                const reason = (type == "message" ? args.slice(1).join(" ") : args[1]) || "N/A";
     
-                update(suggestionMessage, "accepted", reason, member);
+                update(suggestionMessage, "denied", reason, member);
     
                 reply(Embed({
-                    title: lang.AdminModule.Commands.Accept.Suggestion
+                    title: lang.AdminModule.Commands.Deny.Suggestion
                 }));
-                
+
                 return resolve(true);
             }
     
             else if (bugreport) {
                 if (!Utils.hasPermission(member, config.BugReports.ManageBugReportsRole)) {
                     reply(Embed({ preset: "nopermission" }), { ephemeral: true });
+
                     return resolve();
                 }
     
                 const update = require("../../modules/methods/updateBugreport");
                 const bugreportMessage = await guild.channels.cache.get(bugreport.channel)?.messages.fetch(bugreport.message);
-                const reason = args.slice(1).join(" ") || "N/A";
+                const reason = (type == "message" ? args.slice(1).join(" ") : args[1]) || "N/A";
     
-                update(bugreportMessage, "accepted", reason, member);
+                update(bugreportMessage, "denied", reason, member);
     
                 reply(Embed({
-                    title: lang.AdminModule.Commands.Accept.BugReport
+                    title: lang.AdminModule.Commands.Deny.BugReport
                 }));
 
                 return resolve(true);
@@ -65,47 +64,35 @@ module.exports = {
             else if (application) {
                 if (!Utils.hasPermission(member, config.Applications.Reviewers)) {
                     reply(Embed({ preset: "nopermission" }), { ephemeral: true });
+
                     return resolve();
                 }
     
-                const reason = args.slice(1).join(" ") || "N/A";
+                const reason = (type == "message" ? args.slice(1).join(" ") : args[1]) || "N/A";
                 const applyingUser = guild.members.cache.get(application.creator);
                 if (!applyingUser) {
                     reply(Embed({ preset: 'error', description: lang.TicketModule.Commands.Application.Errors.UserLeft }), { ephemeral: true });
+
                     return resolve();
                 }
-    
+
                 const applicationChannel = guild.channels.cache.get(application.channel_id);
+                const embed = Utils.Embed({ title: lang.TicketModule.Commands.Application.Embeds.Denied.Title, description: lang.TicketModule.Commands.Application.Embeds.Denied.Description.replace(/{reason}/g, reason), color: config.EmbedColors.Error });
+                
+                if (config.Applications.DMDecision) applyingUser.send(embed).catch(() => applicationChannel.send(lang.TicketModule.Commands.Application.Errors.CantNotify));
     
-                if (application.status == 'Accepted') {
-                    reply(Embed({ preset: "error", description: lang.TicketModule.Commands.Application.Errors.AlreadyAccepted }), { ephemeral: true });
-                    return resolve();
-                }
-    
-                const positions = config.Applications.Positions;
-                const position = positions[application.rank];
-                if (!position) {
-                    reply(Embed({ preset: 'error', description: lang.TicketModule.Commands.Apply.Errors.PositionNotFound.replace(/{pos}/g, application.rank) }), { ephemeral: true });
-                    return resolve();
-                }
-    
-                if (config.Applications.AddRoleWhenAccepted) {
-                    const role = Utils.findRole(position.Role, guild);
-                    if (!role) reply(Embed({ preset: "error", description: lang.TicketModule.Commands.Apply.Errors.RoleNotFound.replace(/{role}/g, position.Role) }), { ephemeral: true });
-                    else applyingUser.roles.add(role);
-                }
-    
-                const embed = Utils.Embed({ title: lang.TicketModule.Commands.Application.Embeds.Accepted.Title, description: lang.TicketModule.Commands.Application.Embeds.Accepted.Description.replace(/{reason}/g, reason), color: config.EmbedColors.Success });
-                if (config.Applications.DMDecision) applyingUser.send(embed).catch(() => reply(lang.TicketModule.Commands.Application.Errors.CantNotify, { ephemeral: true }));
-    
-                await Utils.variables.db.update.applications.setStatus(applicationChannel.id, 'Accepted');
+                await Utils.variables.db.update.applications.setStatus(applicationChannel.id, 'Denied');
     
                 applicationChannel.send({ content: `<@${applyingUser.id}>`, embeds: embed.embeds });
     
-                let newTopic = config.Applications.Channel.Topic.replace(/{user-tag}/g, applyingUser.user.tag).replace(/{user-id}/g, applyingUser.id).replace(/{position}/g, application.rank).replace(/{status}/g, "Accepted");
+                let newTopic = config.Applications.Channel.Topic.replace(/{user-tag}/g, applyingUser.user.tag).replace(/{user-id}/g, applyingUser.id).replace(/{position}/g, application.rank).replace(/{status}/g, "Denied");
                 await applicationChannel.setTopic(newTopic);
     
-                bot.emit("applicationAccepted", application, member, reason);
+                bot.emit("applicationDenied", application, member, reason);
+
+                reply(Embed({
+                    title: lang.AdminModule.Commands.Deny.Application
+                }));
 
                 return resolve(true);
             }
@@ -113,16 +100,16 @@ module.exports = {
             else {
                 reply(Embed({
                     preset: "error",
-                    description: "That is not a valid suggestion, bug report, or application message ID"
-                }), { ephmeral: true });
+                    description: lang.AdminModule.Commands.Deny.InvalidID
+                }), { ephemeral: true });
 
                 return resolve();
             }    
         });
     },
-    description: "Change a suggestion, bug report, or application to accepted",
-    aliases: ["accepted"],
-    usage: "accept <channel/message ID> <reason>",
+    description: "Change a suggestion, bug report, or application to denied",
+    aliases: ["denied"],
+    usage: "deny <channel/message ID> <reason>",
     arguments: [
         {
             name: "id",
@@ -132,7 +119,7 @@ module.exports = {
         },
         {
             name: "reason",
-            description: "The reason for accepting the suggestion, bug report, or application",
+            description: "The reason for denying the suggestion, bug report, or application",
             required: false,
             type: "STRING"
         }
